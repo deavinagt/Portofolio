d34ff6b87f5c7cd354b982f89a8e69e0d18a51a0
deavinagt <deavinagt@gmail.com>
2024-06-03 15:23:20 +0700

    update

--- script.js
+++ script.js
@@ -3,9 +3,7 @@ function downloadCV() {
     "https://drive.google.com/file/d/1OfvDNmDeLNnTNdtMZJZxtFiI0XvvkSo1/view?usp=drive_link";
 
   window.open(cvURL, "_blank");
-
-  console.log("aduh di klik nich");
-}
+};
 
 document.addEventListener("DOMContentLoaded", function () {
   var downloadBtn = document.querySelector(".btn-download");
@@ -16,9 +14,48 @@ function letsConnect() {
   var lURL = "https://www.linkedin.com/in/deavinagt/";
 
   window.open(lURL, "_blank");
-}
+};
 
 document.addEventListener("DOMContentLoaded", function () {
   var letsConnectBtn = document.querySelector(".btn-letsconnect");
   letsConnectBtn.addEventListener("click", letsConnect);
+});
+
+
+function scrollToTop(){
+  window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+  });
+};
+
+document.addEventListener("DOMContentLoaded", function () {
+  var scrollingToTop = document.querySelector(".scroll-to-top");  
+  scrollingToTop.addEventListener("click",scrollToTop);
+});
+
+function changeHeaderBackground() {
+  const header = document.querySelector('.header');
+  const scrollPosition = window.scrollY;
+
+  if (scrollPosition > 0) {
+    header.style.backgroundColor = '#98bad5';
+    header.classList.add("scrolled");
+  } else {
+    header.style.backgroundColor = 'transparent';
+    header.classList.remove("scrolled");
+  }
+};
+
+document.addEventListener("DOMContentLoaded", function (){
+  document.addEventListener("scroll", changeHeaderBackground);
+});
+
+function SubmitMessage() {
+  alert("Thankyou, Your Message Have Been Submited");
+};
+
+document.addEventListener("DOMContentLoaded", function (){
+  const btnSubmitMessage = document.querySelector('#send-message')
+  btnSubmitMessage.addEventListener("click", SubmitMessage);
 });
